17cbe72c60bc6966f53079ea1eaaff80f8ae67ab
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2022-08-21 09:56:21 +0530

    Add files via upload

---
+++ Java Programming/Operators.java
@@ -0,0 +1,55 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        // First Question
+        /*
+         * int x = 2, y = 5;
+         * int exp1 = (x * y / x);
+         * int exp2 = (x * (y / x))
+         * ;System.out.print(exp1 + " , ");
+         * System.out.print(exp2);
+         */
+
+        // Second Question
+        /*
+         * int x = 200, y = 50, z = 100;
+         * if (x > y && y > z) {
+         * System.out.println("Hello");
+         * }
+         * if (z > y && z < x) {
+         * System.out.println("Java");
+         * }
+         * if ((y + 200) < x && (y + 150) < z) {
+         * System.out.println("Hello Java");
+         * }
+         */
+
+        // Third Question
+        /*
+         * int x, y, z;
+         * x = y = z = 2;
+         * x += y;
+         * y -= z;
+         * z /= (x + y);
+         * System.out.println(x + " " + y + " " + z);
+         */
+
+        // Fourth Question
+        /*
+         * int x = 9, y = 12;
+         * int a = 2, b = 4, c = 6;
+         * int exp = 4 / 3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b *
+         * y);
+         * System.out.println(exp);
+         */
+
+        // Fifth Question
+
+        int x = 10, y = 5;
+        int exp1 = (y * (x / y + x / y));
+        int exp2 = (y * x / y + y * x / y);
+        System.out.println(exp1);
+        System.out.println(exp2);
+
+    }
+}
---
+++ Java Programming/TernaryOperator.java
@@ -0,0 +1,11 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int number = 5;
+         
+
+        //Ternary Operator
+
+       String type = ((number%2)==0) ? "Even" : "Odd";
+       System.out.println(type);
+    }
+}
