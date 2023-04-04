3c497e2225f09d53f11908d680b795b593b2a803
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-04-04 20:07:32 +0530

    Adding Logic Code

--- Java Programming/String/stringBuilder.java
+++ Java Programming/String/stringBuilder.java
@@ -1,5 +1,9 @@
 public class stringBuilder{
     public static void main(String[] args) {
-        
+        StringBuilder sb = new StringBuilder();
+        for(char ch='a'; ch<='z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
     }
 }
