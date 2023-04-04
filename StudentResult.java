f39e7e2ce10d9c0e8d800b34fcddc9aac882f94c
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-04-04 13:09:55 +0530

    Deleted File And Arrange The All File in Java Folder

--- StudentResult.java
+++
@@ -1,8 +0,0 @@
-public class StudentResult {
-    public static void main(String[] args) {
-        int marks = 75;
-
-        String reportCard = marks >= 33 ? "Pass"  : "Fail";
-        System.out.println(reportCard);
-    }
-}                 
