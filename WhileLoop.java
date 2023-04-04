fc21975d82eb12776f969fcf9024557b262600b9
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-04-04 13:06:45 +0530

    Delete WhileLoop.java

--- WhileLoop.java
+++
@@ -1,9 +0,0 @@
-public class WhileLoop{
-    public static void main(String[] args) {
-        int counter = 1;
-        while( counter <= 10){
-            System.out.println(counter);
-            counter++ ;
-        }
-    }
-}
