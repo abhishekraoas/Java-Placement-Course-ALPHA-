b5e9ed381cbe5584437c08bfe2536334a62143a1
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-04-05 11:45:08 +0530

    Changing the class name

--- Java Programming/String/StringChecker.java
+++ Java Programming/String/StringChecker.java
@@ -1,4 +1,4 @@
-public class stringBuilder{
+public class StringChecker{
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         for(char ch='a'; ch<='z'; ch++){
---
+++ Java Programming/String/StringPalindrome.java
@@ -0,0 +1,20 @@
+public class StringPalindrome {
+
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+
+                // not a Palindromme
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String name = "Hello";
+
+        System.out.println(isPalindrome(name));
+    }
+}
